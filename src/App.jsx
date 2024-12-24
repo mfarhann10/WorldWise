05bf866cb26fb43ecceb6293dc181be24f911131
mfarhann10 <mfarhann1009@gmail.com>
2024-12-24 09:31:28 +0700

    add main Page, and implement a react router for 3 components and notfound router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-/* eslint-disable no-case-declarations */
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Homepages from "./pages/Homepages";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col items-center bg-[#343a40] text-[#f1f3f5] p-8">
-      <p>Haloo</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepages />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
