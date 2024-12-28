526b1f765b4c2601827cec4c753e118a5bbd50c9
mfarhann10 <mfarhann1009@gmail.com>
2024-12-28 14:02:33 +0700

    reading and setting query string to store the state in the URL

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,6 +1,25 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Maps</h1>
+      <h1>
+        Position: {lat},{lng}
+      </h1>
+      <button
+        onClick={() => {
+          setSearchParams({ lat: 50, lng: 20 });
+        }}
+      >
+        Change pos
+      </button>
+    </div>
+  );
 }
 
 export default Map;
