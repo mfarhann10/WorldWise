b69b352680dc5bdc59f7e425942297e8e9164dde
mfarhann10 <mfarhann1009@gmail.com>
2025-01-12 20:16:26 +0700

    add a query string to store lat&long in URL

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,10 +1,16 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  useMapEvent,
+} from "react-leaflet";
 import { useEffect, useState } from "react";
 import { useCities } from "../../contexts/CitesContext";
 function Map() {
-  const navigate = useNavigate();
   const { cities } = useCities();
 
   const [mapPosition, setMapPosititon] = useState([40, 0]);
@@ -12,6 +18,7 @@ function Map() {
   const mapLat = searchParams.get("lat");
   const mapLng = searchParams.get("lng");
 
+  //rstore the mapLat & mapLong when the back button got clicked
   useEffect(
     function () {
       if ((mapLat, mapLng)) setMapPosititon([mapLat, mapLng]);
@@ -44,6 +51,7 @@ function Map() {
           </Marker>
         ))}
         <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   );
@@ -53,6 +61,14 @@ function ChangeCenter({ position }) {
   const map = useMap();
   map.setView(position);
   return null;
+}
+
+//navigate to form
+function DetectClick() {
+  const navigate = useNavigate();
+  useMapEvent({
+    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+  });
 }
 
 export default Map;
